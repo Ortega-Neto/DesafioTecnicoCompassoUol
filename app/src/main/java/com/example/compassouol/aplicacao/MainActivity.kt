39ebc078be7078fd687840cadebb42aa1e6b18c5
Luiz Carlos Ortega Neto <luiz.ortega@ufms.com>
2021-05-22 10:59:31 -0400

    22/05 Início do projeto, colocando algumas das dependências

---
+++ app/src/main/java/com/example/compassouol/aplicacao/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.compassouol.aplicacao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.compassouol.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
